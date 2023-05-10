bc275e47b769fa592cf175bd52870c661160e6f0
Ashish Chandel <ashishchandel023@gmail.com>
2023-05-10 22:56:24 +0530

    done assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        //obj.name = "Ashish";
+        //System.out.println(obj.name);
+        //java: name has private access in com.driver.RWOnly
+        obj.setName("Ashish");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    // declare a private vairable
+    private String name;
+
+    String getName(){
+        return this.name;
+    }
+    void setName(String name){
+        this.name = name;
+    }
 }
